e68a8c3a17a7a251d60dcb5a171e5a7164a0c78e
Kai Azim <hi@mrkai77.dev>
2024-11-09 13:19:15 -0700

    Add template iOS Xcode project

---
+++ DeceptICONS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeceptICONS
+//
+//  Created by Kai Azim on 2024-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeceptICONS/DeceptICONSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeceptICONSApp.swift
+//  DeceptICONS
+//
+//  Created by Kai Azim on 2024-11-09.
+//
+
+import SwiftUI
+
+@main
+struct DeceptICONSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
