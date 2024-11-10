e6fd027ddecb609f27f32abb2bf074aa8842f620
nsriv531 <nsriv531@mtroyal.ca>
2024-11-10 11:33:49 -0700

    login styling!

--- src/Components/LoggingIn.js
+++ src/Components/LoggingIn.js
@@ -5,7 +5,6 @@ import { GoogleAuthProvider } from 'firebase/auth';
 
 function LoggingIn() {
   const { currentUser, userLoggedIn, createGoogleUser, loading } = useContext(AuthContext);
-  // const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isSigningIn, setIsSigningIn] = useState(false);
@@ -25,17 +24,8 @@ function LoggingIn() {
   const handleNavigate = (path) => {
     setIsTransitioning(true);
     setTimeout(() => navigate(path), 1000); // Navigate after animation
-  }
+  };
 
-  // const emailSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (!isSigningIn) {
-  //     setIsSigningIn(true);
-  //     await signInWithEmail(email, password);
-  //   }
-  // }
-
-  // seperate submission with Google
   const handleGoogleSignIn = () => {
     const provider = new GoogleAuthProvider(); // Create GoogleAuthProvider instance
     createGoogleUser(provider)
@@ -48,43 +38,46 @@ function LoggingIn() {
   };
 
   return (
-    <div className={`min-h-screen flex items-center justify-center bg-gray-100 bg-gradient-to-r from-lavender-pink to-light-cyan ${isTransitioning ? "animate-fadeOut" : ""}`}>
+    <div className={`min-h-screen flex items-center justify-center bg-gradient-to-r from-lavender-pink to-light-cyan ${isTransitioning ? "animate-fadeOut" : ""}`}>
       { userLoggedIn && (<Navigate to={'/dashboard'} replace={true} />) }
       <form onSubmit={handleSubmit} className={`bg-white p-8 max-w-lg mx-auto rounded-lg shadow-lg ${isTransitioning ? "" : "animate-slideIn"}`}>
-        <h2 className="text-2xl font-bold mb-6 text-gray-800">Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-wenge">Login</h2>
 
-        <label className="block mb-4 font-bold text-gray-700">
+        <label className="block mb-4 font-bold text-wenge">
           Username:
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
-            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-1 block w-full px-4 py-2 border border-orchid-pink rounded-md shadow-sm focus:outline-none focus:ring-orchid-pink focus:border-orchid-pink"
           />
         </label>
 
-        <label className="block mb-4 font-bold text-gray-700">
+        <label className="block mb-4 font-bold text-wenge">
           Password:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-1 block w-full px-4 py-2 border border-orchid-pink rounded-md shadow-sm focus:outline-none focus:ring-orchid-pink focus:border-orchid-pink"
           />
         </label>
 
-      <button
-        type="submit"
-        className="w-full bg-indigo-600 text-white py-2 rounded-md font-semibold hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        Login
-      </button>
+        <button
+          type="submit"
+          className="w-full bg-wenge text-white py-2 rounded-md font-semibold hover:bg-lavender-pink focus:outline-none focus:ring-2 focus:ring-wenge focus:ring-offset-2"
+        >
+          Login
+        </button>
+        
         <button
           onClick={handleGoogleSignIn}
-          className="w-full mt-4 bg-red-600 text-white py-2 rounded-md font-semibold hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-        >Log In with Google</button>
+          className="w-full mt-4 bg-wenge text-white py-2 rounded-md font-semibold hover:bg-lavender-pink focus:outline-none focus:ring-2 focus:ring-wenge focus:ring-offset-2"
+        >
+          Log In with Google
+        </button>
       </form>
     </div>
   );
