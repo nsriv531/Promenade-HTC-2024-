be4ca52f08bea16772c25ca11272eceab8fcfb73
nsriv531 <nsriv531@mtroyal.ca>
2024-11-09 22:42:57 -0700

    adding dashboard

---
+++ src/Components/Footer.js
@@ -0,0 +1,39 @@
+// ButtonComponent.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AiFillHome, AiOutlineUser } from 'react-icons/ai';
+
+const Footer = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      padding: '10px',
+      background: 'linear-gradient(to right, #fbc2eb, #a6c1ee)',
+      borderRadius: '20px',
+      width: '100%',
+      maxWidth: '400px',
+      margin: 'auto',
+    }}>
+      <div
+        style={{ textAlign: 'center', cursor: 'pointer' }}
+        onClick={() => navigate('/')}
+      >
+        <AiFillHome size={30} color="#8b5e83" />
+        <p style={{ color: '#8b5e83', marginTop: '5px' }}>Start</p>
+      </div>
+      <div
+        style={{ textAlign: 'center', cursor: 'pointer' }}
+        onClick={() => navigate('/profile')}
+      >
+        <AiOutlineUser size={30} color="#8b5e83" />
+        <p style={{ color: '#8b5e83', marginTop: '5px' }}>Profile</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
