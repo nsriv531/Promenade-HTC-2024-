2f66e4e542e76bc1708a0daa6c88749849e1ad5e
Jauniew21 <jauniew21@gmail.com>
2024-11-09 19:45:05 -0700

    i hate flexbox. it's looking accurate now

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
   return (
     <div className="App flex h-screen bg-gradient-to-r from-lavender-pink to-light-cyan">
       <header className="App-header m-auto">
-        <h1 className='font-customi text-wenge'>
-          Promenade
-        </h1>
-        <p className='font-custom text-wenge'>
-          Walk together, anytime.
-        </p>
-        <div className="button-group flex items-center justify-center">
-          <button className="App-button" onClick={() => navigate("/login")}>Login</button>
-          <button className="App-button" onClick={() => navigate("/signup")}>Sign Up</button>
+        <div className='py-56'>
+          <h1 className='font-customi text-wenge'>
+            Promenade
+          </h1>
+          <p className='font-inter-medium text-wenge'>
+            Walk together, anytime.
+          </p>
+        </div>
+        <div className="">
+          <button className="font-inter text-wenge bg-transparent h-12 w-80 rounded-xl bg-opacity-45 bg-light-cyan" onClick={() => navigate("/login")}>Continue</button>
         </div>
       </header>
     </div>
