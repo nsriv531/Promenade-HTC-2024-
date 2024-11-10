737a6e2dc712998079f10c33dc41ef3b895ae752
daraakitoyemru <dakit711@mtroyal.ca>
2024-11-09 21:35:49 -0700

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import "./App.css";
 import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div className="App h-screen flex justify-center items-center bg-gradient-to-r from-lavender-pink to-light-cyan">
-      <header className="App-header flex flex-col items-center">
-        <div className='text-center py-44'>
-          <h1 className='font-customi text-wenge'>
-            Promenade
-          </h1>
-          <p className='font-inter-medium text-wenge'>
-            Walk together, anytime.
-          </p>
-        </div>
-        <div className="grid gap-3 grid-rows-2 mt-auto">
-          <button className="font-inter text-wenge h-12 w-80 rounded-xl bg-opacity-45 bg-light-cyan" onClick={() => navigate("/login")}>
-            Sign In
+    <div className="App flex h-screen bg-gradient-to-r from-lavender-pink to-light-cyan">
+      <header className="App-header m-auto">
+        <h1 className="font-customi text-wenge">Promenade</h1>
+        <p className="font-custom text-wenge">Walk together, anytime.</p>
+        <div className="button-group flex items-center justify-center">
+          <button className="App-button" onClick={() => navigate("/login")}>
+            Login
           </button>
-          <button className="font-inter text-wenge h-12 w-80 rounded-xl bg-opacity-45 bg-light-cyan" onClick={() => navigate("/signup")}>
+          <button className="App-button" onClick={() => navigate("/signup")}>
             Sign Up
           </button>
         </div>
