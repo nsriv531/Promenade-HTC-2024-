859c868cdd2a17bacaeb4fe3c0ccd221000e950a
nsriv531 <nsriv531@mtroyal.ca>
2024-11-09 20:47:09 -0700

    yo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import './App.css';
 import { useNavigate } from 'react-router-dom';
 import uploadDataToFirestore from './uploadData';
-=======
-import "./App.css";
-import { useNavigate } from "react-router-dom";
->>>>>>> main
 
 function App() {
   const navigate = useNavigate();
@@ -16,31 +11,17 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Our Platform</h1>
-        <p>
-          Join our community today and explore all the features we offer!
-        </p>
-        <div className="button-group flex items-center justify-center">
-          <button className="App-button" onClick={() => navigate("/login")}>Login</button>
-          <button className="App-button" onClick={() => navigate("/signup")}>Sign Up</button>
-          <button className="App-button" onClick={handleUpload}>Upload Data</button>
-=======
     <div className="App h-screen flex justify-center items-center bg-gradient-to-r from-lavender-pink to-light-cyan">
       <header className="App-header text-center">
-        <div className='py-56 m-auto'>
-          <h1 className='font-customi text-wenge'>
-            Promenade
-          </h1>
-          <p className='font-inter-medium text-wenge'>
-            Walk together, anytime.
-          </p>
+        <div className="py-56 m-auto">
+          <h1 className="font-customi text-wenge">Promenade</h1>
+          <p className="font-inter-medium text-wenge">Walk together, anytime.</p>
         </div>
-        <div className="">
+        <div className="button-group flex items-center justify-center">
           <button className="font-inter text-wenge h-12 w-80 rounded-xl bg-opacity-45 bg-light-cyan" onClick={() => navigate("/login")}>Continue</button>
->>>>>>> main
+        </div>
+        <div className="button-group flex items-center justify-center mt-4">
+          <button className="App-button" onClick={handleUpload}>Upload Data</button>
         </div>
       </header>
     </div>
